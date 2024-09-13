7deef8de4b5c2f7e8ba5c59a7d578899742b51ce
Gustavo Nicoletti <gus.nicoletti@gmail.com>
2024-09-13 17:18:48 -0300

    identificacao

--- corpo.py
+++ corpo.py
@@ -1,1 +1,16 @@
 produtos = []
+
+
+
+
+mev = float('inf')
+mav = -1
+for produto in produtos:
+    if produto[quantidade] > mav:
+        produto[nome] == mav
+    if produto[quantidade] < mev:
+        produto[nome] == mev
+
+
+print(f"produto menos vendido: {mev}")
+print(f"produto mais vendido: {mav}")
